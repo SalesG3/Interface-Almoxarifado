1627827888b0434e2b16a53b531c24f38a0c139c
Sales <contabilgs3@gmail.com>
2024-10-21 16:01:35 -0300

    Desenvolvimento: Pesquisa, Mascara Código, Código Automático, Novo Registro

--- src/app/componentes/almoxarifados/almoxarifados.component.ts
+++ src/app/componentes/almoxarifados/almoxarifados.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-almoxarifados',
@@ -9,6 +10,94 @@ import { Component } from '@angular/core';
 })
 export class AlmoxarifadosComponent {
 
+  // CRUD:
+
+  // Novo Registro:
+  async novoRegistro( ) {
+
+    try {
+    
+      let request = await fetch(environment.APIURL + '/novo/almoxarifados', {
+        method: "POST",
+        headers: {
+          TOKEN: environment.TOKEN,
+          "Content-Type":"applcation/json",
+        },
+        body: JSON.stringify({
+          codigo: (document.querySelector('#codigo') as HTMLInputElement).value,
+          almoxarifado: (document.querySelector('#almoxarifado') as HTMLInputElement).value,
+          descricao: (document.querySelector('#descricao') as HTMLInputElement).value
+        })
+      }).then(response => {if(response.ok){return response.json()} else { console.log(response); return false}});
+
+      if(request.sucesso){
+        this.mudarTela('');
+      }
+
+
+    } catch (erro) {
+      alert('Inconsistência Interna! Favor entrar em contato com Suporte.');
+      console.error(erro);
+    }
+  }
+
+  // OUTROS:
+
+  // Pesquisa: REVISAR APÓS EXISTIR LANÇAMENTOS PARA ENCORPOAR O TBODY
+  async pesquisar ( ) {
+
+    try {
+      let request = await fetch(environment.APIURL + '/pesquisa/almoxarifados', {
+        method: "POST",
+        headers: {
+          TOKEN: environment.TOKEN,
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+          pesquisa: (document.querySelector('#pesquisa') as HTMLInputElement).value
+        })
+      }).then(response => {if(response.ok){return response.json()} else{ console.log(response); return false}})
+
+        
+  
+    } catch (erro) {
+      alert('Inconsistência Interna! Favor entrar em contato com Suporte.');
+      console.error(erro);
+    }
+  }
+  
+
+  // Mascara Código:
+  mascaraCodigo( input : HTMLInputElement){
+
+    let formatado : string = "";
+    for(let i = 0; i < input.value.length; i++){
+      
+    }
+  }
+
+  // Código Automático:
+  async autoCodigo(){
+
+    try {
+      let request = await fetch(environment.APIURL + '/codigo/almoxarifados', {
+        method: "GET",
+        headers: {
+          TOKEN: environment.TOKEN,
+          "Content-Type":"application/json"
+        }
+      }).then(response => {if(response.ok){return response.json()} else { console.log(response); return false}});
+
+      (document.querySelector('#codigo') as HTMLInputElement).value = String(request[0].codigo).padStart(2, '0');
+
+    } catch(erro) {
+      alert('Inconsistência Interna! Favor entrar em contato com Suporte.');
+      console.error(erro);
+    }
+
+  }
+
+  // Alternar entre telas:
   mudarTela( modo : string){
     let crud = document.querySelector('.crud') as HTMLElement;
     let operadores = document.querySelector('.operadores') as HTMLElement;
@@ -18,6 +107,7 @@ export class AlmoxarifadosComponent {
     
     switch( modo ) {
       case 'Incluindo':
+        this.autoCodigo();
         crud.setAttribute('hidden','');
         operadores.style.display = 'inline-block'
 
